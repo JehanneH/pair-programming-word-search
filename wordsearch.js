f27590d5e6e3d8273ae41e5c8692dd9c0aa7bfd1
Jehanne Hodge <samarahjhodge@gmail.com>
2020-06-03 18:04:58 -0400

    check wordsearch test cases

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,36 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    const horizontalJoin = letters.map(ls => ls.join(''));
+
+    if (letters.length === 0) {
+        return false;
     }
+    let found = false; 
+    
+    for (let l of horizontalJoin) {
+        if (l.includes(word)) {
+            return true   
+        }
+    }
+
+    for (let l of transpose(horizontalJoin)) {
+        if (l.join('').includes(word)) {
+            return true   
+        }
+    }
+    return found
 }
+
+const transpose = function(matrix) {
+    let answerArray = [];
+    for (let i = 0; i < matrix[0].length; i++) {
+      let rowArray = [];
+      for (let j = 0; j < matrix.length; j ++) {
+        rowArray.push(matrix[j][i])
+      }
+      answerArray.push(rowArray);
+    }
+  return answerArray;
+  };
+
 
 module.exports = wordSearch
